a87b41a24241a46c326e7fb3f59e3bdf56819d2e
Daniel Falkner <dafalkne@microsoft.com>
2021-02-10 12:40:34 +0100

    add builddate

--- src/js/tabs/firmware_flasher.js
+++ src/js/tabs/firmware_flasher.js
@@ -264,8 +264,8 @@ TABS.firmware_flasher.initialize = function (callback) {
                     var formattedDate = ("0" + date.getDate()).slice(-2) + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + date.getFullYear() + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
                     var descriptor = {
                         "releaseUrl": release.html_url,
-                        "name"      : version + " #" + builddate,
-                        "version"   : version + " #" + builddate,
+                        "name"      : version + builddate,
+                        "version"   : version + builddate,
                         "url"       : asset.browser_download_url,
                         "file"      : asset.name,
                         "target"    : target,
